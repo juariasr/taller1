b35a55a89bb05ddca037c3976cf6c0b5efd8d555
= <=>
2022-04-26 21:22:33 -0500

    Add configuration and .env

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require("http");
+const app = require("./server");
+const config = require("./server/config");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const { port } = config;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end('{"message": "Hello World"}');
-});
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const config = {
+  port: process.env.PORT,
+};
+
+module.exports = config;
