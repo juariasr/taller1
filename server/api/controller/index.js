0e0efb5e09afc62f2af0df651813acd1b2faaa7b
angelo <angelorpa@gmail.com>
2022-04-28 17:23:19 -0500

    Add Update feature

--- server/api/controller/index.js
+++ server/api/controller/index.js
@@ -32,18 +32,48 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-  res.json({ method: "put" });
+  // res.json({ method: "put" });
+  const { body = {} } = req;
+  const { id = -1 } = body;
+
+  if (id === -1) {
+    next({
+      statusCode: 501,
+      message: "Task Id not found in body Data.",
+    });
+  }
+
+  let task = {
+    id: body.id,
+    description: body.description,
+    author: body.author,
+  };
+
+  objIndex = data.findIndex((obj) => obj.id == task.id);
+
+  // Log object to Console.
+  console.log("Before update: ", data[objIndex]);
+
+  // Update object's name property.
+  data[objIndex].description = task.description;
+  data[objIndex].author = task.author;
+  data[objIndex].updatedAt = new Date();
+
+  // Log object to console again.
+  console.log("After update: ", data[objIndex]);
+
+  res.json({ message: "Task Updated" });
 };
 
 exports.delete = (req, res, next) => {
   const { params = {} } = req;
-  const { id = '' } = params;
+  const { id = "" } = params;
 
-  const task1 = data.find(function(item){
+  const task1 = data.find(function (item) {
     return item.author === id;
   });
-  
-  data = data.filter(data => data.author !== id);
- 
-  res.json({message : 'User Delete'});
+
+  data = data.filter((data) => data.author !== id);
+
+  res.json({ message: "Task Deleted" });
 };
--- server/api/index.js
+++ server/api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
 
-router.route("/tasks").get(controller.all).post(controller.post);
-
 router
-  .route("/tasks/:id")
-  .get(controller.get)
-  .put(controller.put)
-  .delete(controller.delete);
+  .route("/tasks")
+  .get(controller.all)
+  .post(controller.post)
+  .put(controller.put);
+
+router.route("/tasks/:id").get(controller.get).delete(controller.delete);
 
 module.exports = router;
