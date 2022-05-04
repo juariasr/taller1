45b7a292df8f75d298c7d12c2fab3f2e06b07afb
angelo <angelorpa@gmail.com>
2022-05-03 20:59:59 -0500

    Adding Structure for versioning the API

--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./controller");
+const controller = require("../../controller");
 
 router
   .route("/tasks")
